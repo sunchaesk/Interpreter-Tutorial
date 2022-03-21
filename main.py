72aa2b8e7d08461343ae98d10ac4226bf7ebe5ab
Chris Kim <sunchaesk@gmail.com>
2022-03-20 21:50:27 -0700

    part 6: edited factor -> ++ LPAREN expr RPAREN

--- main.py
+++ main.py
@@ -4,20 +4,20 @@
 # there is no more input left for lexical analysis
 ##############################################################
 #### #+NOTE
+# PART 6
 # expr: term((PLUS | MINUS) term)*
 # term: factor((MUL | DIV) factor)*
-# factor: INTEGER
+# factor: INTEGER | LPAREN expr RPAREN
+# this
 
-INTEGER, PLUS, MINUS, MUL, DIV, EOF = (
-    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
+    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
 )
 
 
 class Token(object):
     def __init__(self, type, value):
-        # token type: INTEGER, PLUS, MINUS, MUL, DIV, or EOF
         self.type = type
-        # token value: non-negative integer value, '+', '-', '*', '/', or None
         self.value = value
 
     def __str__(self):
@@ -39,7 +39,7 @@ class Token(object):
 
 class Lexer(object):
     def __init__(self, text):
-        # client string input, e.g. "3 * 5", "12 / 3 * 4", etc
+        # client string input, e.g. "4 + 2 * 3 - 6 / 2"
         self.text = text
         # self.pos is an index into self.text
         self.pos = 0
@@ -99,6 +99,14 @@ class Lexer(object):
                 self.advance()
                 return Token(DIV, '/')
 
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+
             self.error()
 
         return Token(EOF, None)
@@ -126,11 +134,19 @@ class Interpreter(object):
     def factor(self):
         """Return an INTEGER token value.
 
-        factor : INTEGER
+        factor : INTEGER | LPAREN expr RPAREN
         """
         token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            result = self.expr()
+            self.eat(RPAREN)
+            return result
+
+
 
     def term(self):
         # term: factor((MUL | DIV) factor)*
